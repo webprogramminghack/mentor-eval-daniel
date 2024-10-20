4f1fe10a1771b7371d4a31a264b2333220653af2
kuma-coffee <daniel099d@gmail.com>
2024-10-20 13:40:25 +0700

    adding api crud

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,21 @@
 import axios from 'axios';
 import { APIConfiguration } from '@/configs/api.config';
 
+interface Data{
+  title: string;
+  completed: boolean;
+}
+
+interface QueryParams {
+  completed?: boolean; 
+  page?: number;      
+  limit?: number;     
+  sort?: string;      
+  order?: 'asc' | 'desc'; 
+  nextCursor?: number;     
+}
+
+
 export const customAxios = axios.create({
   baseURL: APIConfiguration.baseURL,
   headers: {
@@ -8,3 +23,52 @@ export const customAxios = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export const fetchData = async (endpoint:string, params:QueryParams) => {
+  try {
+    const response = await customAxios.get(endpoint, {
+      params:{
+        completed: params.completed,
+        page: params.page,
+        limit: params.limit,
+        sort: params.sort,
+        order: params.order,
+        nextCursor: params.nextCursor,
+      }
+    });
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; 
+  }
+};
+
+export const createData = async (endpoint:string, data:Data) => {
+  try {
+    const response = await customAxios.post(endpoint, data);
+    return response.data; 
+  } catch (error) {
+    console.error('Error creating data:', error);
+    throw error;
+  }
+};
+
+export const updateData = async (endpoint:string, data:Data) => {
+  try {
+    const response = await customAxios.put(endpoint, data);
+    return response.data; 
+  } catch (error) {
+    console.error('Error updating data:', error);
+    throw error;
+  }
+};
+
+export const deleteData = async (endpoint:string) => {
+  try {
+    const response = await customAxios.delete(endpoint);
+    return response.data; 
+  } catch (error) {
+    console.error('Error deleting data:', error);
+    throw error;
+  }
+};
